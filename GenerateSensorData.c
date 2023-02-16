ef0e29091fef50c244b6537d62c35f6fb54a671e
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2023-02-16 23:43:53 +0530

    Update GenerateSensorData.c

--- GenerateSensorData.c
+++ GenerateSensorData.c
@@ -9,14 +9,46 @@ const unsigned int UpperBoundaryCurrent = 100U;
 static tyBatteryParams_t BattParamBuffer[BufferSizePerSession];
 tyBatteryParams_t* const BattParamPointer = BattParamBuffer;
 
+static void CopyFromFileToBuffer(FILE* fp);
+
 void GenerateRandParams(void)
 {
-  
+  for(unsigned char ArrIndex = 0U; ArrIndex < BufferSizePerSession; ++ArrIndex)
+  {
+    BattParamPointer[ArrIndex].BattVoltage = \
+      (float)((rand() % (UpperBoundaryVoltage - LowerBoundaryVoltage + 1)) + LowerBoundaryVoltage)/10.0f;
+    
+    BattParamPointer[ArrIndex].BattVoltage = (float)(rand() % UpperBoundaryVoltage)/10.0f;
+  }
 }
 
 void GetSensorParamsFromFile(void)
 {
-
+  FILE* fp = fopen("SensorDataFile.txt","r");
+  if(fp != NULL)
+  {
+    CopyFromFileToBuffer(fp);
+  }
+  else
+  {
+    printf("Unable to Open the file,check if the file exist \n");
+  }
 }
 
+const tyBatteryParams_t* GetSensorArrayParameters(void)
+{
+  return BattParamPointer;
+}
+
+static void CopyFromFileToBuffer(FILE* fp)
+{
+  char TempBuffer[20];
+  for(unsigned char Index = 0U; ((Index < BufferSizePerSession) || (!feof(fp))) ; ++Index)
+  {
+    fgets(TempBuffer,20,fp);
+    BattParamPointer[Index].BattVoltage = (float)atof(TempBuffer);
+    fgets(TempBuffer,20,fp);
+    BattParamPointer[Index].BattCurrent = (float)atof(TempBuffer);
+    fgets(TempBuffer,20,fp);
+   }
 }
