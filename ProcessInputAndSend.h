57fe9ba52b5b56e10a5882a43ef71000cbd4d980
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2023-02-20 23:07:45 +0530

    Update ProcessInputAndSend.h

--- ProcessInputAndSend.h
+++ ProcessInputAndSend.h
@@ -5,6 +5,14 @@
 #include "GenerateSensorData.h"
 #include <stdio.h>
 
+enum enumParamType {
+  PARAM_VOLTAGE,
+  PARAM_CURRENT
+}
+
+void ChangeTheGetParamType(void (*GetTypeFunction)());
+float GetElement(const unsigned int ArrayElement,enumRequestParam Parameter);
+
 void SendDataToConsole(void);
 
 #endif
